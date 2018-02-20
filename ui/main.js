ab42465e84088c2a1501ebfc9388635ebe264e5b
nikilsivakumar <nikildevikumar@gmail.com>
2018-02-20 14:25:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';    
+
+//for moving the image
+
+var img = document.getElementById('madi');
+img.onclick = function(){
+  img.style.marginleft = '100px';  
+};
